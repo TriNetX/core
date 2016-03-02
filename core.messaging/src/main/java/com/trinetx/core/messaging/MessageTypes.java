ab51c34704aad2465311a41c88a70746d238a3cf
Wai Cheng <wai.cheng@trinetx.com>
2016-03-02 14:59:53 -0500

    Added RpcQueue support for category and name calls

--- core.messaging/src/main/java/com/trinetx/core/messaging/MessageTypes.java
+++ core.messaging/src/main/java/com/trinetx/core/messaging/MessageTypes.java
@@ -13,10 +13,12 @@ public final class MessageTypes {
         public static final String CONCEPT_COUNTS_RESOURCE = "concept_counts_resource";
         public static final String LAB_VALUES_RESOURCE = "lab_values_resource";
         public static final String PROVIDER_COUNTS_RESOURCE = "provider_counts_resource";
+        public static final String CATEGORY_AND_NAME_RESOURCE = "category_and_name_resource";
 
         // TODO: move to TSParam once it is move out of pubsub
         public class TSParam {
             public static final String CONCEPT_COUNTS_REQUEST = "concept_counts_request"; 
+            public static final String CATEGORY_AND_NAME_TERMS = "category_and_name_terms"; 
             public static final String AGE_GENDER_QUALIFIER = "ageGenderQualifier";
             public static final String PROVIDER_SUPPORT_FOR_QUALIFICATION = "providerSupport";
             public static final String EXPORT = "export";
