2a236ef43285b95f822c167d807016d30d71949f
Wai Cheng <wai.cheng@trinetx.com>
2016-03-02 09:01:38 -0500

    Added RpcQueue support for ageValues calls

--- core.messaging/src/main/java/com/trinetx/core/messaging/MessageTypes.java
+++ core.messaging/src/main/java/com/trinetx/core/messaging/MessageTypes.java
@@ -7,6 +7,7 @@ public final class MessageTypes {
         public static final String TERM_RESOURCE = "term_resource";
         public static final String TERM_COUNT_RESOURCE = "term_count_resource";
         public static final String AGE_VALUES_RESOURCE = "age_values_resource";
+        public static final String AGE_COUNTS_RESOURCE = "age_counts_resource";
     }
     
 }
