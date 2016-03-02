6a846c1caa1b27c4d3e3bb6b1ab9b8b36cc3781a
Wai Cheng <wai.cheng@trinetx.com>
2016-03-02 10:06:42 -0500

    Added RpcQueue support for concept hierarchy calls

--- core.messaging/src/main/java/com/trinetx/core/messaging/MessageTypes.java
+++ core.messaging/src/main/java/com/trinetx/core/messaging/MessageTypes.java
@@ -8,6 +8,8 @@ public final class MessageTypes {
         public static final String TERM_COUNT_RESOURCE = "term_count_resource";
         public static final String AGE_VALUES_RESOURCE = "age_values_resource";
         public static final String AGE_COUNTS_RESOURCE = "age_counts_resource";
+        public static final String CONCEPT_HIERARCHY_RESOURCE = "concept_hierarchy_resource";
+        public static final String CONCEPT_HIERARCHY_CHILDREN_RESOURCE = "concept_hierarchy_children_resource";
     }
     
 }
