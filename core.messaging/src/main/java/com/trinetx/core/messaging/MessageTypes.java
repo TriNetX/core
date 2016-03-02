101d973e39fa8f56cd55da08e842cf2d2d3e8c19
Wai Cheng <wai.cheng@trinetx.com>
2016-03-02 12:02:27 -0500

    Added RpcQueue support for concept counts and lab values calls

--- core.messaging/src/main/java/com/trinetx/core/messaging/MessageTypes.java
+++ core.messaging/src/main/java/com/trinetx/core/messaging/MessageTypes.java
@@ -10,6 +10,9 @@ public final class MessageTypes {
         public static final String AGE_COUNTS_RESOURCE = "age_counts_resource";
         public static final String CONCEPT_HIERARCHY_RESOURCE = "concept_hierarchy_resource";
         public static final String CONCEPT_HIERARCHY_CHILDREN_RESOURCE = "concept_hierarchy_children_resource";
+        public static final String CONCEPT_COUNTS_RESOURCE = "concept_counts_resource";
+        public static final String CONCEPT_COUNTS_RESOURCE_ConceptCountsRequest = "concept_counts_request"; // TODO: move to TSParam once it is move out of pubsub
+        public static final String LAB_VALUES_RESOURCE = "lab_values_resource";
     }
     
 }
