c6b4e3b373eb35985c7496f0def5a83e9770461d
Wai Cheng <wai.cheng@trinetx.com>
2016-03-02 14:15:21 -0500

    Added RpcQueue support for provider count  and minor changes to lab values calls

--- core.messaging/src/main/java/com/trinetx/core/messaging/MessageTypes.java
+++ core.messaging/src/main/java/com/trinetx/core/messaging/MessageTypes.java
@@ -11,8 +11,16 @@ public final class MessageTypes {
         public static final String CONCEPT_HIERARCHY_RESOURCE = "concept_hierarchy_resource";
         public static final String CONCEPT_HIERARCHY_CHILDREN_RESOURCE = "concept_hierarchy_children_resource";
         public static final String CONCEPT_COUNTS_RESOURCE = "concept_counts_resource";
-        public static final String CONCEPT_COUNTS_RESOURCE_ConceptCountsRequest = "concept_counts_request"; // TODO: move to TSParam once it is move out of pubsub
         public static final String LAB_VALUES_RESOURCE = "lab_values_resource";
+        public static final String PROVIDER_COUNTS_RESOURCE = "provider_counts_resource";
+
+        // TODO: move to TSParam once it is move out of pubsub
+        public class TSParam {
+            public static final String CONCEPT_COUNTS_REQUEST = "concept_counts_request"; 
+            public static final String AGE_GENDER_QUALIFIER = "ageGenderQualifier";
+            public static final String PROVIDER_SUPPORT_FOR_QUALIFICATION = "providerSupport";
+            public static final String EXPORT = "export";
+        }
     }
     
 }
